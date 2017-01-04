8c3492a569eeac763d607ee8e5a6a493ea46db3a
Kevin Chen <kevinchn@cs.berkeley.edu>
2017-01-04 21:25:51 +0800

    Remove hflags for now.

--- exec_src/Main.hs
+++ exec_src/Main.hs
@@ -3,11 +3,14 @@
 import Blockchain.Output
 import Control.Monad.Logger
 import Executable.EthAnalyzer
-import HFlags
+-- import HFlags
 
-defineFlag "jrpcServer" ("127.0.0.1" :: String)
-  "Ethereum json-rpc server address."
-defineFlag "jrpcPort" (8545 :: Int) "Ethereum json-rpc server port."
+-- defineFlag "jrpcServer" ("127.0.0.1" :: String)
+--   "Ethereum json-rpc server address."
+-- defineFlag "jrpcPort" (8545 :: Int) "Ethereum json-rpc server port."
+
+flags_jrpcServer = "127.0.0.1"
+flags_jrpcPort = 8545
 
 main :: IO ()
 main = do
