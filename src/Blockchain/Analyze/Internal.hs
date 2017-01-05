dfb5a683c545111ea173ed824ed34650cf5c34f2
Kevin Chen <kevinchn@cs.berkeley.edu>
2017-01-05 17:39:55 +0800

    Use HTTP-based JSON-RPC instead. TCP based won't work.

--- src/Blockchain/Analyze/Internal.hs
+++ src/Blockchain/Analyze/Internal.hs
@@ -10,7 +10,8 @@ import Data.Aeson.Types hiding (Error)
 import Data.ByteString.Char8 as DBC
 import Data.Conduit.Network
 import Data.Text as T
-import Network.JsonRpc
+import Network.HTTP.Simple as NHS
+import Network.JsonRpc as NJ
 
 data Req = Web3_clientVersionReq deriving (Show, Eq)
 
@@ -35,27 +36,40 @@ instance FromResponse Res where
 instance ToJSON Res where
   toJSON (Web3_clientVersionRes result) = toJSON result
 
-handleResponse :: Maybe (Either ErrorObj Res) -> Res
-handleResponse t =
-  case t of
-    Nothing -> error "could not receive or parse response"
-    Just (Left e) -> error $ fromError e
-    Just (Right r) -> r
+jrpcVersion :: (MonadIO m) => String -> Int -> m Text
+jrpcVersion server port =
+  initReq <- NHS.parseRequest ("POST http://" ++ server ++ ":" ++ (show port))
+  let req = NHS.setRequestBodyJSON
+      (NJ.buildRequest V2 Web3_clientVersionReq 1) initReq
+  resp <- NHS.httpJSON req
+  case getResponseStatus resp of
+    NHS.ok200 ->
+      case fromResponse "web3_clientVersion" (getResponseBody resp) of
+          Just $ Web3_clientVersionRes ver -> return ver
+          Nothing -> error $ "couldn't get version from response: " ++ (show resp)
+    other -> error $ "non-OK status when getting version: " ++ (show other)
 
-jrpcVersion_ :: MonadLoggerIO m => JsonRpcT m Res
-jrpcVersion_ = do
-  $(logDebug) "debugging response parsing"
-  $(logDebug) $ T.pack $ "toJSON: " ++ (show $ toJSON $ Web3_clientVersionRes "versssss")
-  $(logDebug) $ T.pack $ "parsing: " ++ (show ((fromResponse "web3_clientVersion" $ Response V2 (toJSON $ Web3_clientVersionRes "versssss") (IdInt 1)) :: Maybe Res))
-  -- $(logDebug) $ T.pack $ "fromResponse toJSON: " ++ (toJSON $ Web3_clientVersionRes "versssss")
-  $(logDebug) "sending web3_clientVersion request"
-  tEM <- sendRequest Web3_clientVersionReq
-  return $ handleResponse tEM
+-- handleResponse :: Maybe (Either ErrorObj Res) -> Res
+-- handleResponse t =
+--   case t of
+--     Nothing -> error "could not receive or parse response"
+--     Just (Left e) -> error $ fromError e
+--     Just (Right r) -> r
+
+-- jrpcVersion_ :: MonadLoggerIO m => JsonRpcT m Res
+-- jrpcVersion_ = do
+--   $(logDebug) "debugging response parsing"
+--   $(logDebug) $ T.pack $ "toJSON: " ++ (show $ toJSON $ Web3_clientVersionRes "versssss")
+--   $(logDebug) $ T.pack $ "parsing: " ++ (show ((fromResponse "web3_clientVersion" $ Response V2 (toJSON $ Web3_clientVersionRes "versssss") (IdInt 1)) :: Maybe Res))
+--   -- $(logDebug) $ T.pack $ "fromResponse toJSON: " ++ (toJSON $ Web3_clientVersionRes "versssss")
+--   $(logDebug) "sending web3_clientVersion request"
+--   tEM <- sendRequest Web3_clientVersionReq
+--   return $ handleResponse tEM
 
 
-jrpcVersion :: (MonadLoggerIO m, MonadBaseControl IO m) => String -> Int -> m Text
-jrpcVersion server port = do
-  $logInfo $ T.pack $ "Conntectin to " ++ server ++ ":" ++ show port
-  Web3_clientVersionRes ver <- jsonRpcTcpClient V2 True (
-    clientSettings port $ DBC.pack server) jrpcVersion_
-  return ver
+-- jrpcVersion :: (MonadLoggerIO m, MonadBaseControl IO m) => String -> Int -> m Text
+-- jrpcVersion server port = do
+--   $logInfo $ T.pack $ "Conntectin to " ++ server ++ ":" ++ show port
+--   Web3_clientVersionRes ver <- jsonRpcTcpClient V2 True (
+--     clientSettings port $ DBC.pack server) jrpcVersion_
+--   return ver
