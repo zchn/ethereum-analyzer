93fc919fc41fa6110ea9fffe79624e423b534d43
Kevin Chen <kevin.zchn@gmail.com>
2017-02-07 00:36:48 -0500

    Include line number in decompile.

--- src/Blockchain/Analyze.hs
+++ src/Blockchain/Analyze.hs
@@ -6,27 +6,26 @@ module Blockchain.Analyze
   ) where
 
 import Blockchain.Data.Code
+import Blockchain.ExtWord
 import Blockchain.Util
 import Blockchain.VM.Code
 import Blockchain.VM.Opcodes
 import Data.ByteString
 import Data.HexString
 
-import qualified Data.ByteString.Char8 as DBC
-
-decompileHexString :: ByteString -> [Operation]
+decompileHexString :: ByteString -> [(Word256, Operation)]
 decompileHexString = decompileBS . toBytes . hexString
 
-decompile :: Code -> [Operation]
+decompile :: Code -> [(Word256, Operation)]
 decompile (Code bs) = decompileBS bs
 decompile _ = []
 
-decompileBS :: ByteString -> [Operation]
-decompileBS bs = let hardlimit = 10000 in decompileBSAt bs hardlimit
+decompileBS :: ByteString -> [(Word256, Operation)]
+decompileBS bs = let hardlimit = 10000 in decompileBSAt bs 0 hardlimit
 
-decompileBSAt :: ByteString -> Int -> [Operation]
-decompileBSAt "" _ = []
-decompileBSAt _ 0 = []
-decompileBSAt bs limit =
-  [op] ++ decompileBSAt (safeDrop next bs) (limit-1)
+decompileBSAt :: ByteString -> Word256 -> Int -> [(Word256, Operation)]
+decompileBSAt "" _ _ = []
+decompileBSAt _ _ 0 = []
+decompileBSAt bs base limit =
+  (base+next, op) : decompileBSAt (safeDrop next bs) (base+next) (limit-1)
   where (op, next) = getOperationAt' bs 0
