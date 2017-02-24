3330681691b25c0a958dd06ded65335a0c62a1c3
Kevin Chen <kevin.zchn@gmail.com>
2017-02-23 23:01:07 -0500

    Connect all pieces together, use the latest hoopl

--- src/Blockchain/Analyze/CfgAugmentPass.hs
+++ src/Blockchain/Analyze/CfgAugmentPass.hs
@@ -8,11 +8,9 @@ import Compiler.Hoopl
 import Data.Maybe
 import Data.Set
 
-doCfgAugmentPass :: HplBody -> WordLabelMapM HplBody
-doCfgAugmentPass = error "Unimplemented(doCfgAugmentPass)."
-
 type StackTopFact = Maybe (Set Word256)
 
+stackTopLattice :: DataflowLattice (Maybe (Set Word256))
 stackTopLattice =
   DataflowLattice
   { fact_name = "stackTopLattice"
@@ -30,3 +28,43 @@ joinStackTopLattice _ (OldFact _) (NewFact Nothing) = (SomeChange, Nothing)
 joinStackTopLattice _ (OldFact oldF) (NewFact newF) =
   let joinedF = Just $ unions $ catMaybes [oldF, newF]
   in (changeIf $ joinedF /= oldF, joinedF)
+
+stackTopTransfer :: FwdTransfer HplOp StackTopFact
+stackTopTransfer = mkFTransfer3 coT ooT ocT
+  where
+    coT :: HplOp C O -> StackTopFact -> StackTopFact
+    coT = error "Unimplemented"
+    ooT :: HplOp O O -> StackTopFact -> StackTopFact
+    ooT = error "Unimplemented"
+    ocT :: HplOp O C -> StackTopFact -> FactBase StackTopFact
+    ocT = error "Unimplemented"
+
+cfgAugmentRewrite :: FwdRewrite (CheckingFuelMonad SimpleUniqueMonad) HplOp StackTopFact
+cfgAugmentRewrite = mkFRewrite3 coR ooR ocR
+  where
+    coR :: HplOp C O
+        -> StackTopFact
+        -> SimpleFuelMonad (Maybe (Graph HplOp C O))
+    coR = error "Unimplemented"
+    ooR :: HplOp O O
+        -> StackTopFact
+        -> SimpleFuelMonad (Maybe (Graph HplOp O O))
+    ooR = error "Unimplemented"
+    ocR :: HplOp O C
+        -> StackTopFact
+        -> SimpleFuelMonad (Maybe (Graph HplOp O C))
+    ocR = error "Unimplemented"
+
+cfgAugmentPass :: FwdPass (CheckingFuelMonad SimpleUniqueMonad) HplOp (Maybe (Set Word256))
+cfgAugmentPass =
+  FwdPass
+  { fp_lattice = stackTopLattice
+  , fp_transfer = stackTopTransfer
+  , fp_rewrite = cfgAugmentRewrite
+  }
+
+doCfgAugmentPass :: Label -> HplBody -> SimpleUniqueMonad HplBody
+doCfgAugmentPass entry body =
+  runWithFuel
+    1000000
+    (fst <$> analyzeAndRewriteFwdBody cfgAugmentPass entry body noFacts)
