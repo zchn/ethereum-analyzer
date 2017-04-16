f79e71a483f2a568944be05042c6649b0a8bc30d
Kevin Chen <kevin.zchn@gmail.com>
2017-04-16 17:01:38 -0400

    add blockapps-data

---
+++ blockapps-data/README.md
@@ -0,0 +1,7 @@
+#blockapps-data
+
+[![BlockApps logo](http://blockapps.net/img/logo_cropped.png)](http://blockapps.net)
+
+This repository is a library of common data structures for the blockapps blockchain projects.
+
+
---
+++ blockapps-data/src/Blockchain/Data/Code.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module Blockchain.Data.Code where
+
+import qualified Data.ByteString as B
+import GHC.Generics
+
+import Blockchain.Data.RLP
+
+data Code =
+  Code{codeBytes::B.ByteString}
+  | PrecompiledCode Int deriving (Show, Eq, Read, Ord, Generic)
+
+instance RLPSerializable Code where
+    rlpEncode (Code bytes) = rlpEncode bytes
+    rlpEncode (PrecompiledCode _) = error "Error in call to rlpEncode for Code: Precompiled contracts can not be serialized."
+    rlpDecode = Code . rlpDecode
+
+-- instance Format Code where
+--    format Code {codeBytes=c} = B.unpack c
