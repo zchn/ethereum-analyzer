c6f58d148e7e1298d11da011b997ea7402aea929
Kevin Chen <kevin.zchn@gmail.com>
2017-04-23 20:47:52 -0400

    Implement ea-bytecode-vis.

--- ethereum-analyzer-cli/src/Ethereum/Executable/BytecodeVisMain.hs
+++ ethereum-analyzer-cli/src/Ethereum/Executable/BytecodeVisMain.hs
@@ -5,9 +5,19 @@ module Ethereum.Executable.BytecodeVisMain
     ) where
 
 import Control.Monad.Logger
-import Data.Text
-import Protolude
+import Data.String.ToString
+import Data.Text (null)
+import Ethereum.Analyzer.Disasm
+import Ethereum.Analyzer.Util
+import Protolude hiding (null)
 
 bytecodeVisMain :: Text -> Text -> LoggingT IO ()
-bytecodeVisMain _ _ = -- hexstring dotFilepath =
-  error "Unimplemented."
+bytecodeVisMain hexstring dotFilepath = do
+  hs <- if null hexstring
+        then lift getLine
+        else return hexstring
+  let (ctorDot, dispDot) = disasmToDotText2 $ EvmHexString hs
+  if null dotFilepath
+    then putText dispDot
+    else do lift $ writeFile (toString dotFilepath) dispDot
+            lift $ writeFile (toString $ dotFilepath <> "ctor.dot") ctorDot
