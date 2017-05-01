09c31e38f472e8cc794adf2a1dff7ab655f66a79
Kevin Chen <kevin.zchn@gmail.com>
2017-04-30 21:41:10 -0400

    Implemented a simple cmd console.

--- ethereum-analyzer-webui/web/src/root.js
+++ ethereum-analyzer-webui/web/src/root.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import EAClient from './ea-client.js';
 
 class App extends React.Component {
     constructor(props) {
@@ -22,13 +23,17 @@ class App extends React.Component {
 
 class MenuBar extends React.Component {
     render() {
-        return <div id="ea-menu-bar">There will be a menu here</div>;
+        return (
+            <div id="ea-menu-bar">There will be a menu here</div>
+        );
     }
 }
 
 class LineNavBar extends React.Component {
     render() {
-        return <div id="ea-line-nav-bar">||CCCCCCCCCC|DDDD||</div>;
+        return (
+            <div id="ea-line-nav-bar">||CCCC-----=========================</div>
+        );
     }
 }
 
@@ -44,11 +49,23 @@ class MainBody extends React.Component {
 }
 
 class CmdWindow extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { cmdHistory: [] };
+
+        this.handleNewCmd = this.handleNewCmd.bind(this);
+    }
+
+    handleNewCmd(cmd) {
+        this.setState({ cmdHistory: this.state.cmdHistory.concat([ cmd ])});
+    }
+
     render() {
         return (
             <div id="ea-cmd-window">
-              <CmdOutput />
-              <CmdInput />
+              <CmdOutput cmdHistory={this.state.cmdHistory} />
+              <CmdInput onNewCmd={this.handleNewCmd} />
             </div>
         );
     }
@@ -56,23 +73,48 @@ class CmdWindow extends React.Component {
 
 class CmdOutput extends React.Component {
     render() {
+        // console.log(this.props.cmdHistory);
+        var rows = [];
+        for(var i = 0; i < this.props.cmdHistory.length; i++) {
+            rows.push(<li key={i}>{this.props.cmdHistory[i]}</li>);
+        }
         return (
-            <div id="ea-cmd-output">
-              command input and output history <br />
-              command input and output history <br />
-              command input and output history <br />
-              command input and output history <br />
-              command input and output history <br />
-              command input and output history <br />
-            </div>
+            <ul id="ea-cmd-output" >
+              {rows}
+            </ul>
         );
     }
 }
 
 class CmdInput extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { cmd: "" };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleEnter = this.handleEnter.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({ cmd: e.target.value });
+    }
+
+    handleEnter(e) {
+        if (e.charCode == 13 || e.keyCode == 13) {
+            e.preventDefault();
+            this.props.onNewCmd(this.state.cmd);
+            this.setState({ cmd: "" });
+        }
+    }
+
     render() {
         return (
-            <div id="ea-cmd-input">cmd input</div>
+            <input type="text"
+                   id="ea-cmd-input"
+                   value={this.state.cmd}
+                   onChange={this.handleChange}
+                   onKeyPress={this.handleEnter} />
         );
     }
 }
