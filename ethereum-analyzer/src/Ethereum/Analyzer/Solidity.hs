9b5270fc483bc68217b856d0bf36a6e3ca428f04
Kevin Chen <kevin.zchn@gmail.com>
2017-08-12 19:15:11 -0400

    Merge Pretty.hs, add initial s2s* functions in Simple.hs

--- ethereum-analyzer/src/Ethereum/Analyzer/Solidity.hs
+++ ethereum-analyzer/src/Ethereum/Analyzer/Solidity.hs
@@ -1,9 +1,7 @@
 module Ethereum.Analyzer.Solidity
   ( module Ethereum.Analyzer.Solidity.AstJson
-  , module Ethereum.Analyzer.Solidity.Pretty
   , module Ethereum.Analyzer.Solidity.Simple
   ) where
 
 import Ethereum.Analyzer.Solidity.AstJson
-import Ethereum.Analyzer.Solidity.Pretty
 import Ethereum.Analyzer.Solidity.Simple
--- ethereum-analyzer/src/Ethereum/Analyzer/Solidity/Simple.hs
+++ ethereum-analyzer/src/Ethereum/Analyzer/Solidity/Simple.hs
@@ -10,6 +10,8 @@ module Ethereum.Analyzer.Solidity.Simple
 
 import Protolude hiding (show)
 
+import Ethereum.Analyzer.Solidity.AstJson
+
 data Contract = Contract
   { cName :: Text
   , cStateVars :: [VarDecl]
@@ -31,6 +33,7 @@ data VarType
   | Address
   | Mapping VarType
             VarType
+  | Unknown Text
 
 data FunDefinition = FunDefinition
   { fName :: Idfr
@@ -46,9 +49,49 @@ data Statement
   | StIf Idfr
          [Statement]
          [Statement]
+  | Unsupported SolNode
 
 data Expression
   = BinOp Text
           Idfr
           Idfr
   | ExpLiteral Text
+
+s2sContracts :: SolNode -> [Contract]
+s2sContracts SolNode { name = Just "SourceUnit"
+                      , children = Just sChildren } =
+  concat (map s2sContracts sChildren)
+s2sContracts SolNode { name = Just "ContractDefinition"
+                     , children = Just vChildren
+                     , attributes = Just SolNode { name = Just cName }} =
+  let (vars, funs) = s2sVarsFuns vChildren in
+    [Contract cName vars funs]
+  where s2sVarsFuns :: [SolNode] -> ([VarDecl], [FunDefinition])
+        s2sVarsFuns [] = ([], [])
+        s2sVarsFuns (h : t) = let (vars', funs') = s2sVarsFuns t in
+          (s2sVarDecls h <> vars', s2sFuns h <> funs')
+s2sContracts _ = []
+
+s2sVarDecls :: SolNode -> [VarDecl]
+s2sVarDecls SolNode { name = Just "VariableDeclaration"
+                    , attributes = Just SolNode { name = Just vName,
+                                                  _type = Just vType }} = [VarDecl (Idfr vName) (Unknown vType)]
+s2sVarDecls SolNode { name = Just "ParameterList"
+                    , children = Just pChildren } = concat (map s2sVarDecls pChildren)
+s2sVarDecls _ = []
+
+s2sFuns :: SolNode -> [FunDefinition]
+s2sFuns SolNode { name = Just "FunctionDefinition"
+                , children = Just [ params, returns, body ]
+                , attributes = Just SolNode { name = Just fName }} =
+  [FunDefinition (Idfr fName)
+    (s2sVarDecls params) (s2sVarDecls returns) (s2sStatements body)]
+s2sFuns _ = []
+
+s2sStatements :: SolNode -> [Statement]
+s2sStatements SolNode { name = Just "Block"
+                      , children = Just sChildren } =
+  concat (map s2sStatements sChildren)
+s2sStatements e@SolNode { name = Just "ExpressionStatement"
+                        , children = Just sChildren } = [Unsupported e]
+s2sStatements s = [Unsupported s]
