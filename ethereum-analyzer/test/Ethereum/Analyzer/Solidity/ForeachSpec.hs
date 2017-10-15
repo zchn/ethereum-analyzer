117a5158a7b553a0d8dc802adae8f13d6f170690
Kevin Chen <kevin.zchn@gmail.com>
2017-10-15 01:30:08 -0400

    hindent

--- ethereum-analyzer/test/Ethereum/Analyzer/Solidity/ForeachSpec.hs
+++ ethereum-analyzer/test/Ethereum/Analyzer/Solidity/ForeachSpec.hs
@@ -11,14 +11,14 @@ import Test.Hspec
 spec :: Spec
 spec = do
   describe "statementsOf" $ do
-      it "works for storageJson" $ do
-        case decodeContracts storageJson of
-          Right contracts ->
-            sum (map (length . statementsOf) contracts) `shouldBe` 2
-          Left err -> expectationFailure $ toS err
+    it "works for storageJson" $ do
+      case decodeContracts storageJson of
+        Right contracts ->
+          sum (map (length . statementsOf) contracts) `shouldBe` 2
+        Left err -> expectationFailure $ toS err
   describe "expressionsOf" $ do
-      it "works for storageJson" $ do
-        case decodeContracts storageJson of
-          Right contracts ->
-            sum (map (length . expressionsOf) contracts) `shouldBe` 1
-          Left err -> expectationFailure $ toS err
+    it "works for storageJson" $ do
+      case decodeContracts storageJson of
+        Right contracts ->
+          sum (map (length . expressionsOf) contracts) `shouldBe` 1
+        Left err -> expectationFailure $ toS err
