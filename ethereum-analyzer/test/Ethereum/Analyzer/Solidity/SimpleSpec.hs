605ae92ac66ff2a39c9779e164584854b650677c
Kevin Chen <kevin.zchn@gmail.com>
2017-10-15 21:10:03 -0400

    hlint

--- ethereum-analyzer/test/Ethereum/Analyzer/Solidity/SimpleSpec.hs
+++ ethereum-analyzer/test/Ethereum/Analyzer/Solidity/SimpleSpec.hs
@@ -17,51 +17,51 @@ import qualified Text.PrettyPrint.GenericPretty as GP
 
 spec :: Spec
 spec = do
-  describe "e2h" $ do
+  describe "e2h" $
     it "parses storageJson" $ do
-      let eitherContracts = do
-            solNodes <- decodeSoleNodes (toS storageJson)
-            let mContracts = mapM s2sContracts solNodes
-            let contracts = concat $ runSimpleUniqueMonad mContracts
-            return contracts
-      -- GP.pp eitherContracts
-      eitherContracts `shouldBe`
-        Right
-          [ Contract
-            { cName = "SimpleStorage"
-            , cStateVars =
-                [VarDecl {vName = Idfr "storedData", vType = Unknown "uint256"}]
-            , cFunctions =
-                [ FunDefinition
-                  { fName = Idfr "set"
-                  , fParams =
-                      [VarDecl {vName = Idfr "x", vType = Unknown "uint256"}]
-                  , fReturns = []
-                  , fBody =
-                      [ StAssign
-                          (JustId (Idfr "storedData"))
-                          (ExpLval (JustId (Idfr "x")))
-                      ]
-                  }
-                , FunDefinition
-                  { fName = Idfr "get"
-                  , fParams = []
-                  , fReturns =
-                      [VarDecl {vName = Idfr "", vType = Unknown "uint256"}]
-                  , fBody = [StReturn [JustId (Idfr "storedData")]]
-                  }
-                ]
-            }
-          ]
+    let eitherContracts = do
+          solNodes <- decodeSoleNodes (toS storageJson)
+          let mContracts = mapM s2sContracts solNodes
+          let contracts = concat $ runSimpleUniqueMonad mContracts
+          return contracts
+    -- GP.pp eitherContracts
+    eitherContracts `shouldBe`
+      Right
+        [ Contract
+          { cName = "SimpleStorage"
+          , cStateVars =
+              [VarDecl {vName = Idfr "storedData", vType = Unknown "uint256"}]
+          , cFunctions =
+              [ FunDefinition
+                { fName = Idfr "set"
+                , fParams =
+                    [VarDecl {vName = Idfr "x", vType = Unknown "uint256"}]
+                , fReturns = []
+                , fBody =
+                    [ StAssign
+                        (JustId (Idfr "storedData"))
+                        (ExpLval (JustId (Idfr "x")))
+                    ]
+                }
+              , FunDefinition
+                { fName = Idfr "get"
+                , fParams = []
+                , fReturns =
+                    [VarDecl {vName = Idfr "", vType = Unknown "uint256"}]
+                , fBody = [StReturn [JustId (Idfr "storedData")]]
+                }
+              ]
+          }
+        ]
   describe "Simple" $ mapM_ nonEmptySimple testFilepaths
 
 nonEmptySimple :: Text -> SpecWith ()
-nonEmptySimple filepath = do
+nonEmptySimple filepath =
   context (toS filepath) $
-    it "converts SolNode to Contracts" $ do
-      contracts <-
-        do solNodes <- solNodesOf filepath
-           let mContracts = mapM s2sContracts solNodes
-           let contracts = concat $ runSimpleUniqueMonad mContracts
-           return contracts
-      length contracts `shouldSatisfy` (0 <)
+  it "converts SolNode to Contracts" $ do
+    contracts <-
+      do solNodes <- solNodesOf filepath
+         let mContracts = mapM s2sContracts solNodes
+         let contracts = concat $ runSimpleUniqueMonad mContracts
+         return contracts
+    length contracts `shouldSatisfy` (0 <)
