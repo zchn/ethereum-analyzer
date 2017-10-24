0cf090fbcb7046729a29ddfe3411188ff4b8de29
Kevin Chen <kevin.zchn@gmail.com>
2017-10-23 21:01:56 -0400

    Make Hoople.hs compile.

--- ethereum-analyzer/src/Ethereum/Analyzer/Solidity/Hoople.hs
+++ ethereum-analyzer/src/Ethereum/Analyzer/Solidity/Hoople.hs
@@ -1,30 +1,46 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+
 module Ethereum.Analyzer.Solidity.Hoople
     ( hoopleOf
     ) where
 
-import Compiler.Hoople
+import Protolude hiding ((<*>))
+
+import Compiler.Hoopl
 import Ethereum.Analyzer.Solidity.Simple
 
 data HContract = HContract
   { hcName :: Text
   , hcStateVars :: [VarDecl]
   , hcFunctions :: [HFunDefinition]
-  } deriving (Eq, Generic, Show, GP.Out)
+  }
 
 
-type CFG = Graph HStatement O C
+type CFG = AGraph HStatement O C
 
 data HFunDefinition = HFunDefinition
   { hfName :: Idfr
   , hfParams :: [VarDecl]
   , hfReturns :: [VarDecl]
   , hfCFG :: CFG
-  } deriving (Eq, Generic, Show, GP.Out)
+  }
 
 data HStatement e x where
   CoSt :: Label -> HStatement C O
   OoSt :: Statement -> HStatement O O
   OcSt :: Statement -> [Label] -> HStatement O C
+  OcJump :: Label -> HStatement O C
+
+instance NonLocal HStatement where
+  entryLabel (CoSt lbl) = lbl
+  successors (OcSt _ lbls) = lbls
+  successors (OcJump lbl) = [lbl]
+
+instance HooplNode HStatement where
+  mkBranchNode = OcJump
+  mkLabelNode = CoSt
 
 hoopleOf :: UniqueMonad m => Contract -> m HContract
 hoopleOf Contract { cName = name
@@ -41,37 +57,38 @@ hfunOf FunDefinition { fName = name
                      , fBody = stmts } = do
   entryL <- freshLabel
   exitL <- freshLabel
-  cfg <- mkBranch entryL <*> mkLabel entryL <*> graphOf stmts exitL entryL
-  return $ HFunDefinition name params returns lbl cfg
+  stmtGraph <- graphOf stmts exitL entryL
+  let cfg = (mkBranch entryL |*><*| mkLabel entryL <*> stmtGraph) :: CFG
+  return $ HFunDefinition name params returns cfg
 
 graphOf :: UniqueMonad m => [Statement] -> Label -> Label -> m CFG
 graphOf [] exitL _ = return $ mkBranch exitL
-graphOf h:t exitL loopL = do
+graphOf (h:t) exitL loopL = do
   postDom <- graphOf t exitL loopL
   case h of
-  | StLocalVarDecl _ ->  return $ mkMiddle' h <*> postDom
-  | StAssign _ _ -> return $ mkMiddle' h <*> postDom
-  | StDelete _ -> return $ mkMiddle' h <*> postDom
-  | StTodo _ -> return $ mkMiddle' h <*> postDom
-  | StReturn _ -> do
+    StLocalVarDecl _ ->  return $ mkMiddle' h <*> postDom
+    StAssign _ _ -> return $ mkMiddle' h <*> postDom
+    StDelete _ -> return $ mkMiddle' h <*> postDom
+    StTodo _ -> return $ mkMiddle' h <*> postDom
+    StReturn _ -> do
       pdL <- freshLabel
       return $ mkLast (OcSt h []) |*><*| (mkLabel pdL <*> postDom)
-  | StThrow -> do
+    StThrow -> do
       pdL <- freshLabel
       return $ mkLast (OcSt h []) |*><*| (mkLabel pdL <*> postDom)
-  | StIf _ thenSts elseSts -> do
+    StIf _ thenSts elseSts -> do
       pdL <- freshLabel
       thenG <- graphOf thenSts pdL loopL
       elseG <- graphOf elseSts pdL loopL
       -- TODO(zchn): remove the then and else statements in h when
       -- constructing OcSt.
-      ifte = mkIfthenElse (\thenLbl elseLbl -> mkLast (OcSt h [thenLbl, elseLbl])) thenG elseG
+      let ifte = mkIfThenElse (\thenLbl elseLbl -> mkLast (OcSt h [thenLbl, elseLbl])) thenG elseG
       return $ ifte |*><*| (mkLabel pdL <*> postDom)
-  | StLoop stmts -> do
+    StLoop stmts -> do
       pdL <- freshLabel
       bodyLbl <- freshLabel
       bodyG <- graphOf stmts pdL bodyLbl
       return $ mkBranch bodyLbl |*><*| (mkLabel bodyLbl <*> bodyG) |*><*| (mkLabel pdL <*> postDom)
-  | StBreak -> return $ mkBranch exitL
-  | StContinue -> return $ mkBranch loopL
+    StBreak -> return $ mkBranch exitL
+    StContinue -> return $ mkBranch loopL
   where mkMiddle' a = mkMiddle (OoSt a)
